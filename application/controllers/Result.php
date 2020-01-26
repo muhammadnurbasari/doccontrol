3adc87ff62ef727a27c99b3c5b2cf613676214eb
m.nurbasari@gmail.com <suithehe29>
2020-01-26 22:21:35 +0700

    doc_release

--- application/controllers/Result.php
+++ application/controllers/Result.php
@@ -356,6 +356,9 @@ class Result extends CI_Controller {
 		if ($parameter == '') {
 			$data['title'] = 'Release Document Propose';
 			$cek_department = $this->Result_model->get_name_by_id('department', $this->session->userdata('user')[0]['department_id'], 'department_code');
+			if ($cek_department != 'QA' && $this->session->userdata('user')[0]['level_id'] != 3 ) {
+				$this->db->where('department_id', $this->session->userdata('user')[0]['department_id']);
+			}
 			$data['doc_release_headers'] = $this->Result_model->getData('doc_release_header');
 			$data['table'] = 'doc_release_header';
 			$this->templating('doc_release_header/index', $data);
