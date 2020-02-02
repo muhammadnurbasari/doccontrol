95df92ef207b34fdb7de0806018d3cb92f7b0926
m.nurbasari@gmail.com <suithehe29>
2020-02-02 21:06:45 +0700

    Update index.php

--- application/views/back/distributions/index.php
+++ application/views/back/distributions/index.php
@@ -219,35 +219,35 @@ input:checked + .slider:before {
               confirm.push(result[1][i].department_id);
             }
             var html = `<div class="widget-box">
-          <div class="widget-title"> <span class="icon"><i class="icon-time"></i></span>
-            <h5>To Do List</h5>
-          </div>
-          <div class="widget-content nopadding">
-            <table class="table table-striped table-bordered">
-              <thead>
-                <tr>
-                  <th>Department</th>
-                  <th>Status</th>
-                </tr>
-              </thead>
-              <tbody>`;
+                          <div class="widget-title"> <span class="icon"><i class="icon-time"></i></span>
+                            <h5>To Do List</h5>
+                          </div>
+                          <div class="widget-content nopadding">
+                            <table class="table table-striped table-bordered">
+                              <thead>
+                                <tr>
+                                  <th>Department</th>
+                                  <th>Status</th>
+                                </tr>
+                              </thead>
+                              <tbody>`;
             for (var i = 0; i < result[0].length; i++) {
               if (jQuery.inArray(result[0][i].department_id, confirm) != -1) {
                 html += `<tr>
-                  <td class="taskDesc">`+result[0][i].department_code+`</td>
-                  <td class="taskStatus"><span class="in-progress">CONFIRMED</span></td>
-                </tr>`
+                          <td class="taskDesc">`+result[0][i].department_code+`</td>
+                          <td class="taskStatus"><span class="in-progress">CONFIRMED</span></td>
+                        </tr>`
               } else {
                 html += `<tr>
-                    <td class="taskDesc">`+result[0][i].department_code+`</td>
-                    <td class="taskStatus"><span class="pending">NO CONFIRMED</span></td>
-                  </tr>`
+                          <td class="taskDesc">`+result[0][i].department_code+`</td>
+                          <td class="taskStatus"><span class="pending">NO CONFIRMED</span></td>
+                        </tr>`
               }
             }
-            html += `</tbody>
-            </table>
-          </div>
-        </div>`
+                html += `</tbody>
+                        </table>
+                      </div>
+                    </div>`
             $('.modal-body').html(html)
           }
         })
