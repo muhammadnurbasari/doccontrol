7bf3f3c8e7671ba736c226daf8849e4ece2496cb
m.nurbasari@gmail.com <suithehe29>
2020-02-07 23:45:29 +0700

    Update index.php

--- application/views/back/report/index.php
+++ application/views/back/report/index.php
@@ -9,7 +9,63 @@
 <!--End-breadcrumbs-->
 <?php //var_dump($this->session->userdata()); ?>
   <div class="container-fluid">   
-      
+      <div class="widget-box">
+        <div class="widget-title"> <span class="icon"> <i class="icon-align-justify"></i> </span>
+          <h5>Form Elements</h5>
+        </div>
+        <div class="widget-content nopadding">
+          <form class="form-horizontal">
+            <div class="control-group">
+              <label class="control-label">Color picker (hex)</label>
+              <div class="controls">
+                <input type="text" data-color="#ffffff" value="#ffffff" class="colorpicker input-big span11">
+                <span class="help-block">Color picker with Formate of  (hex)</span> </div>
+            </div>
+            <div class="control-group">
+              <label class="control-label">Color picker (rgba)</label>
+              <div class="controls">
+                <input type="text" data-color="rgba(344,232,53,0.5)" value="rgba(344,232,53,0.5)" data-color-format="rgba" class="colorpicker span11">
+                <span class="help-block">Color picker with Formate of  (rgba)</span> </div>
+            </div>
+            <div class="control-group">
+              <label class="control-label">Date picker (dd-mm)</label>
+              <div class="controls">
+                <input type="text" data-date="01-02-2013" data-date-format="dd-mm-yyyy" value="01-02-2013" class="datepicker span11">
+                <span class="help-block">Date with Formate of  (dd-mm-yy)</span> </div>
+            </div>
+            <div class="control-group">
+              <label class="control-label">Date Picker (mm-dd)</label>
+              <div class="controls">
+                <div  data-date="12-02-2012" class="input-append date datepicker">
+                  <input type="text" value="12-02-2012"  data-date-format="mm-dd-yyyy" class="span11" >
+                  <span class="add-on"><i class="icon-th"></i></span> </div>
+              </div>
+            </div>
+            <div class="control-group">
+              <label class="control-label">Color Picker (rgb)</label>
+              <div class="controls">
+                <div data-color-format="rgb" data-color="rgb(155, 142, 180)" class="input-append color colorpicker colorpicker-rgb">
+                  <input type="text" value="rgb(155, 142, 180)" class="span11">
+                  <span class="add-on"><i style="background-color: rgb(155, 142, 180)"></i></span> </div>
+              </div>
+            </div>
+            <div class="control-group">
+              <label class="control-label">Color Picker (hex)</label>
+              <div class="controls">
+                <div data-color-format="hex" data-color="#000000"  class="input-append color colorpicker">
+                  <input type="text" value="#000000" class="span11">
+                  <span class="add-on"><i style="background-color: #000000"></i></span> </div>
+              </div>
+            </div>
+            <div class="form-actions">
+              <button type="submit" class="btn btn-success">Save</button>
+              <button type="submit" class="btn btn-primary">Reset</button>
+              <button type="submit" class="btn btn-info">Edit</button>
+              <button type="submit" class="btn btn-danger">Cancel</button>
+            </div>
+          </form>
+        </div>
+      </div>
     <hr/>
   </div>  
 </div>
