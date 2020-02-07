dc772f15c0a8bae662da575ea1b216badccae5da
m.nurbasari@gmail.com <suithehe29>
2020-02-07 23:33:19 +0700

    Update index.php

--- application/views/back/master_doc_list/index.php
+++ application/views/back/master_doc_list/index.php
@@ -87,10 +87,7 @@
                         <a target="_BLANK" href="<?php echo base_url('assets/files/release/'.$value['doc_file']) ?>">
                           <span class="badge tombol badge-warning" data-id="<?php echo $value[$table.'_id']; ?>"><i class="icon-info-sign"></i> view & download</span>
                         </a>
-                        <!-- <span class="badge tombol badge-success edit" data-id="<?php echo $value[$table.'_id']; ?>"><i class="icon-edit"></i></span>
-                        <?php if ($value['doc_status'] == 3) { ?>
-                          <span class="badge tombol badge-important hapus" data-toggle="modal" data-target="#deletemodal" data-id="<?php echo $value[$table.'_id']; ?>" data-name="<?php echo $value['doc_release_code']; ?>"><i class="icon-trash"></i></span>
-                        <?php } ?> -->
+                        <span class="badge tombol badge-success details-distributions" data-toggle="modal" data-target="#modalDetailDistributions" data-id="<?php echo $value[$table.'_id']; ?>"><i class="icon-edit"></i> details distribution</span>
                       </td>  
                     </tr>
                   <?php endforeach ?>
@@ -104,5 +101,70 @@
 </div>
 
 <!--end-main-container-part-->
+
+      <div class="modal fade" id="modalDetailDistributions" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Confirm Distributions Details</h5>
+            </div>
+            <div class="modal-body"></div>
+          </div>
+        </div>
+      </div>
+
+<script type="text/javascript">
+  $(document).ready(function() {
+
+    // ajax modal
+    $('.details-distributions').each(function(index, value) {
+      $(this).click(function() {
+        var id = $(this).data('id');
+        $.ajax({
+          url : '<?php echo base_url('result/distributions/details') ?>'+'/'+id,
+          success : function(response) {
+            var result = JSON.parse(response);
+            var confirm = [];
+            for (var i = 0; i < result[1].length; i++) {
+              confirm.push(result[1][i].department_id);
+            }
+            var html = `<div class="widget-box">
+                          <div class="widget-title"> <span class="icon"><i class="icon-time"></i></span>
+                            <h5>To Do List</h5>
+                          </div>
+                          <div class="widget-content nopadding">
+                            <table class="table table-striped table-bordered">
+                              <thead>
+                                <tr>
+                                  <th>Department</th>
+                                  <th>Status</th>
+                                </tr>
+                              </thead>
+                              <tbody>`;
+            for (var i = 0; i < result[0].length; i++) {
+              if (jQuery.inArray(result[0][i].department_id, confirm) != -1) {
+                html += `<tr>
+                          <td class="taskDesc">`+result[0][i].department_code+`</td>
+                          <td class="taskStatus"><span class="in-progress">CONFIRMED</span></td>
+                        </tr>`
+              } else {
+                html += `<tr>
+                          <td class="taskDesc">`+result[0][i].department_code+`</td>
+                          <td class="taskStatus"><span class="pending">NO CONFIRMED</span></td>
+                        </tr>`
+              }
+            }
+                html += `</tbody>
+                        </table>
+                      </div>
+                    </div>`
+            $('.modal-body').html(html)
+          }
+        })
+      })
+    })
+
+  })
+</script>
 
 
